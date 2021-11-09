30f1ea1642a66bd70708590e39d29b347d9b3153
anilkumar769gmail <anilkumar769@gmail.com>
2021-11-09 13:06:21 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-p1w2m8
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-p1w2m8)
---
+++ src/app/app.component.ts
@@ -0,0 +1,33 @@
+import { Component, VERSION } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent  {
+  num1: number = 5;
+  num2: number = 6;
+  active: boolean =true;
+
+  color: string =`blue ${this.num1}`;
+
+  students: String[] =["a"];
+  complexStudents: {}[] = [{one:"two"}];
+
+  result: number = this.num1 + this.num2;
+
+  speak():string{
+
+    return 'hello there!!'
+  }
+
+colors: string[] =["Blue", "red"];
+
+flights = [
+{origin:"Denva", destination: "miami"},
+{origin:"NYC", destination: "San Frnacisco"},
+{origin:"NJ", destination: "Oakland"},
+];
+
+}
